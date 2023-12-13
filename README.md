5ea5a3b137366b42f9d23875cccc2963ddd088a1
Jupegarnica <juan@geekshubs.com>
2023-12-13 16:42:46 +0100

    Update Deno installation command

---
+++ README.md
@@ -0,0 +1,22 @@
+# Nombre del Proyecto
+
+Descripción del proyecto.
+
+## Requisitos
+
+- Deno
+
+## Instalación
+
+Para instalar el proyecto, ejecuta el siguiente comando:
+
+```sh
+deno install --allow-read --allow-net -fn downtime cli.ts
+```
+
+## Uso
+
+```sh
+downtime https://garn.dev http://faker.deno.dev --sleep 2000 --timeout 3000
+
+```
--- ui.tsx
+++ ui.tsx
@@ -10,18 +10,19 @@ function App({ urls, timeout, sleep }: { urls: string[], timeout: number, sleep:
     const infoColor = "grey";
     const [totalTime, setTotalTime] = useState(0);
     useEffect(() => {
+        const INTERVAL = totalTime < 60_000 ? 1000 : 60_000;
         const interval = setInterval(() => {
             setTotalTime(Date.now() - startTime);
-        }, 1000);
+        }, INTERVAL);
         return () => clearInterval(interval);
-    },[]);
+    }, []);
     return (
         <Box gap={1} flexDirection="column">
-            <Box flexDirection="row" gap={2}>
+            <Box flexDirection="row" gap={1} paddingTop={1}>
                 <Text color={infoColor} dimColor>From:</Text><Text color={infoColor}>{new Date(startTime).toLocaleString()}</Text>
-                <Text color={infoColor} dimColor>Sleep:</Text><Text color={infoColor}>{ms(sleep)}</Text>
-                <Text color={infoColor} dimColor>Timeout:</Text><Text color={infoColor}>{ms(timeout)}</Text>
-                <Text color={infoColor} dimColor>Total time:</Text><Text color={infoColor}>{ms(totalTime)}</Text>
+                <Text color={infoColor} dimColor>Total:</Text><Text color={infoColor}>{ms(totalTime)}</Text>
+                <Text color={infoColor} dimColor>--sleep</Text><Text color={infoColor}>{ms(sleep)}</Text>
+                <Text color={infoColor} dimColor>--timeout</Text><Text color={infoColor}>{ms(timeout)}</Text>
             </Box>
             <Box gap={1} flexDirection="column" >
                 {urls.map((url) => (
@@ -38,7 +39,11 @@ function UrlMonitor({ url, timeout, sleep }: { url: string, key?: string, timeou
     try {
         new URL(url);
     } catch (error) {
-        return <Text color="red">Invalid URL: {url}</Text>;
+        return (
+            <Box flexDirection="column" borderStyle="round" borderColor="blackBright" padding={1}>
+                <Text color="red">{error.message}</Text>
+            </Box>
+        )
     }
     const [downTimeElapsed, setDownTimeElapsed] = useState(0);
     const [upTimeElapsed, setUpTimeElapsed] = useState(0);
@@ -55,7 +60,7 @@ function UrlMonitor({ url, timeout, sleep }: { url: string, key?: string, timeou
             for await (const data of generator) {
                 setDownTimeElapsed(data.totalDownTimeElapsed);
                 setUpTimeElapsed(data.totalUpTimeElapsed);
-                setStatus(data.lastResponse?.status);
+                setStatus(data.lastResponse?.status || 0);
                 setErrorMessages(data.lastError?.message);
             }
         })();
@@ -65,23 +70,46 @@ function UrlMonitor({ url, timeout, sleep }: { url: string, key?: string, timeou
         };
     }, [url]);
 
+
+
     return (
-        // <Text>
-        //     {url} {'\n'}
-        //     {"=>"} downtime: {ms(downTimeElapsed)} {'\n'}
-        //     {"=>"} uptime: {ms(upTimeElapsed)} {'\n'}
-        //     {status && `=>  status: ${status}`}
-        //     {!status && `=>  error: ${errorMessages}`}
-        // </Text>
-        <Box flexDirection="column">
-            <Text color="blue">{url}</Text>
-            <Box gap={1}><Text dimColor>{"=>"} downtime:</Text><Text>{ms(downTimeElapsed)}</Text></Box>
-            <Box gap={1}><Text dimColor>{"=>"} uptime:</Text><Text>{ms(upTimeElapsed)}</Text></Box>
-            {status && <Text color="green">{`=>  status: ${status}`}</Text>}
-            {!status && <Text color="red">{`=>  error: ${errorMessages}`}</Text>}
+
+        <Box flexDirection="column" borderStyle="round" borderColor="blackBright" padding={1}>
+            <Box flexDirection="row" gap={1}>
+                <Box flexDirection="row" gap={1}>
+                    {
+                        status ?
+                            <Text color={statusColor(status)}>►  {status}</Text> :
+                            <Text color="red">🛑 ---</Text>
+                    }
+
+                </Box>
+                <Text color="cyan">{url}</Text>
+                <Box gap={1}>
+                    <Text color='whiteBright'>{ms(downTimeElapsed)}</Text>
+                    <Text color="gray">{"downtime"}</Text>
+                </Box>
+                <Box gap={1}>
+                    <Text color='whiteBright'>{ms(upTimeElapsed)}</Text>
+                    <Text color="gray">{"uptime  "}</Text>
+                </Box>
+            </Box>
+            {
+                errorMessages
+                    ? <Box paddingTop={1} paddingLeft={3}><Text color="red">{errorMessages}</Text></Box>
+                    : null
+            }
+
         </Box>
     );
 
+}
+
+function statusColor(status: number) {
+    if (status >= 200 && status < 300) return "green";
+    if (status >= 300 && status < 400) return "yellow";
+    if (status >= 400 && status < 500) return "red";
+    return "red";
 }
 
 
