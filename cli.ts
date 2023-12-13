d940c19bcc9bdb4f7287a3c5a880ec1637e8ecfc
Jupegarnica <juan@geekshubs.com>
2023-12-13 14:42:42 +0100

    Update CLI and UI components

--- cli.ts
+++ cli.ts
@@ -4,11 +4,15 @@ import { parseArgs } from "https://deno.land/std@0.208.0/cli/parse_args.ts";
 const args = parseArgs(Deno.args);
 const urls = args._.map(String);
 const sleep = args.sleep ? Number(args.sleep) : 1000;
-const timeout = args.sleep ? Number(args.timeout) : 5000;
+const timeout = args.timeout ? Number(args.timeout) : 5000;
 const maxTime = args.maxTime ? Number(args.maxTime) : 0
 
 
 if (import.meta.main) {
+    if (urls.length === 0) {
+        console.error("No URL provided");
+        Deno.exit(1);
+    }
     const { unmount } = renderUI(urls, sleep, timeout);
 
     Deno.addSignalListener("SIGINT", () => {
--- ui.tsx
+++ ui.tsx
@@ -1,14 +1,35 @@
 import React from 'npm:react';
-import { render, Text } from 'npm:ink';
+import { render, Text, Box } from 'npm:ink';
 import { ms } from "https://deno.land/x/ms@v0.1.0/ms.ts";
 import { checkDownTime } from "./main.ts";
 
 const { useState, useEffect } = React;
-
+const startTime = Date.now();
 function App({ urls, timeout, sleep }: { urls: string[], timeout: number, sleep: number }) {
 
-
-    return urls.map((url) => <UrlMonitor key={url} url={url} timeout={timeout} sleep={sleep} />);
+    const infoColor = "grey";
+    const [totalTime, setTotalTime] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTotalTime(Date.now() - startTime);
+        }, 1000);
+        return () => clearInterval(interval);
+    },[]);
+    return (
+        <Box gap={1} flexDirection="column">
+            <Box flexDirection="row" gap={2}>
+                <Text color={infoColor} dimColor>From:</Text><Text color={infoColor}>{new Date(startTime).toLocaleString()}</Text>
+                <Text color={infoColor} dimColor>Sleep:</Text><Text color={infoColor}>{ms(sleep)}</Text>
+                <Text color={infoColor} dimColor>Timeout:</Text><Text color={infoColor}>{ms(timeout)}</Text>
+                <Text color={infoColor} dimColor>Total time:</Text><Text color={infoColor}>{ms(totalTime)}</Text>
+            </Box>
+            <Box gap={1} flexDirection="column" >
+                {urls.map((url) => (
+                    <UrlMonitor key={url} url={url} timeout={timeout} sleep={sleep} />
+                ))}
+            </Box>
+        </Box>
+    )
 
 }
 
@@ -28,7 +49,7 @@ function UrlMonitor({ url, timeout, sleep }: { url: string, key?: string, timeou
         const controller = new AbortController();
         const { signal } = controller;
 
-        const generator = checkDownTime(url, { signal });
+        const generator = checkDownTime(url, { signal, timeout, sleep });
 
         (async () => {
             for await (const data of generator) {
@@ -45,19 +66,26 @@ function UrlMonitor({ url, timeout, sleep }: { url: string, key?: string, timeou
     }, [url]);
 
     return (
-        <Text>
-            {url} {'\n'}
-            {"=>"} downtime: {ms(downTimeElapsed)} {'\n'}
-            {"=>"} uptime: {ms(upTimeElapsed)} {'\n'}
-            {status && `=>  status: ${status}`}
-            {!status && `=>  error: ${errorMessages}`}
-        </Text>
+        // <Text>
+        //     {url} {'\n'}
+        //     {"=>"} downtime: {ms(downTimeElapsed)} {'\n'}
+        //     {"=>"} uptime: {ms(upTimeElapsed)} {'\n'}
+        //     {status && `=>  status: ${status}`}
+        //     {!status && `=>  error: ${errorMessages}`}
+        // </Text>
+        <Box flexDirection="column">
+            <Text color="blue">{url}</Text>
+            <Box gap={1}><Text dimColor>{"=>"} downtime:</Text><Text>{ms(downTimeElapsed)}</Text></Box>
+            <Box gap={1}><Text dimColor>{"=>"} uptime:</Text><Text>{ms(upTimeElapsed)}</Text></Box>
+            {status && <Text color="green">{`=>  status: ${status}`}</Text>}
+            {!status && <Text color="red">{`=>  error: ${errorMessages}`}</Text>}
+        </Box>
     );
 
 }
 
 
 
-export function renderUI(urls: string[], timeout: number, sleep: number) {
+export function renderUI(urls: string[], sleep: number, timeout: number) {
     return render(<App urls={urls} timeout={timeout} sleep={sleep} />);
 }
